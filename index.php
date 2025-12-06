229c53a53f5d3a075edc40712d4395327240baab
sagarkashyap9835 <vidyasagar983531@gmail.com>
2025-12-06 22:18:20 +0530

    nnn

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php 
-echo "hello world"
+echo "hello world";
+print "hello radha";  //ye v chalega
+?>
+<?php
+$age=20;
+echo $age;
+?>
+
+<?php
+$price=.22222;
+echo $price;
+?>
+<?php
+$name="sagar";
+echo $name;
+?>
+
+<?php 
+$greeting="hello";
+echo "Message:$greeting\n";
+echo 'message:$greeting';
 ?>
